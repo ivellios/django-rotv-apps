1265568292803e84559e7cfbf2f9ae898c5ca595
Janusz Kamieński <janusz.kamienski@gmail.com>
2017-11-25 19:25:26 +0100

    form updated and fixed bugs

--- rotv_apps/partners/views.py
+++ rotv_apps/partners/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Create your views here.
+from django.urls import reverse
 from django.views import generic
 
 from .forms import MediaPatronageForm
@@ -9,3 +10,9 @@ from .models import MediaPatronage
 class MediaPatronageRequestView(generic.CreateView):
     model = MediaPatronage
     form_class = MediaPatronageForm
+
+    def get_success_url(self):
+        return reverse('partners_event_request_confirm')
+
+class MediaPatronageRequestConfirmView(generic.TemplateView):
+    template_name = 'partners/confirm.html'
