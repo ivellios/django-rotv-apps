e0b09d0d28b28345e965cc01e4ed7bb41d8b81b6
Janusz Kamieński <janusz.kamienski@gmail.com>
2018-01-14 17:32:51 +0100

    further patronage admin extension + admin action for activation

--- rotv_apps/partners/admin.py
+++ rotv_apps/partners/admin.py
@@ -5,12 +5,23 @@ from django.contrib import admin
 from models import Partner, MediaPatron, MediaPatronage, NormalMediaPatronage, Colaborator
 
 
+def activate_patronage(modeladmin, request, queryset):
+    queryset.update(active=True)
+
+
+activate_patronage.short_description = u'Oznacz wybrane wydarzenia jako aktywne'
+
+
 class MediaPatronageAdmin(admin.ModelAdmin):
-    list_display = ['name', 'start', 'end', 'active', 'activated', 'created', 'modified']
+    list_display = ['name', 'city', 'spot',
+                    'start', 'end', 'active', 'activated',
+                    'contact_email',
+                    'created', 'modified']
     
 
 class NormalMediaPatronageAdmin(admin.ModelAdmin):
     list_display = ['name', 'start', 'end', 'active']
+
 
 admin.site.register(MediaPatronage, MediaPatronageAdmin)
 admin.site.register(NormalMediaPatronage, NormalMediaPatronageAdmin)
