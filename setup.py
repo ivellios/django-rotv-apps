82867ada32bb717b77bf2312b23162e060e1f776
Janusz Kamienski <janusz.kamienski@deployed.pl>
2018-06-03 20:32:15 +0200

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rotv-apps',
-    version='2.3.1',
+    version='2.4.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',  # example license
