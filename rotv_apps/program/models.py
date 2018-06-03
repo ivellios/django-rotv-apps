3f71e93aae80103b39d586f913e53ecc77009851
Janusz Kamienski <janusz.kamienski@deployed.pl>
2018-06-03 11:01:09 +0200

    added m2m separatelly

--- rotv_apps/program/models.py
+++ rotv_apps/program/models.py
@@ -79,7 +79,7 @@ class Playlist(models.Model):
 
 
 class PlaylistEpisode(models.Model):
-    # episode = models.ForeignKey('program.Episode', related_name='playlist_episodes')
+    episode = models.ForeignKey('program.Episode', related_name='playlist_episodes')
     playlist = models.ForeignKey('program.Playlist', related_name='playlist_episodes')
     position = models.PositiveSmallIntegerField(_('Position'), null=True)
 
