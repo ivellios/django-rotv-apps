9d69d2354fd52284e33f88a718c6435b431e6673
Janusz Kamienski <janusz.kamienski@deployed.pl>
2018-06-03 11:14:45 +0200

    enable playlists

--- rotv_apps/program/models.py
+++ rotv_apps/program/models.py
@@ -89,8 +89,6 @@ class PlaylistEpisode(models.Model):
             ('episode', 'playlist', 'position'),
         )
 
-
-
     def __unicode__(self):
         return self.playlist.name
 
@@ -137,7 +135,7 @@ class EpisodeManager(models.Manager):
 class Episode(models.Model):
     added = models.DateTimeField(_(u'Data dodania'), auto_now_add=True)
     program = models.ForeignKey(Program, null=True, blank=True)
-    # playlist = models.ManyToManyField('program.Playlist', through='program.PlaylistEpisode')
+    playlist = models.ManyToManyField('program.Playlist', through='program.PlaylistEpisode')
     number = models.IntegerField(_(u'Numer odcinka'), blank=True, null=True)
     hosts = models.ManyToManyField(Host, verbose_name=_(u'Prowadzący'), blank=True,)
     title = models.CharField(_(u'Tytuł odcinka'), max_length=255)
